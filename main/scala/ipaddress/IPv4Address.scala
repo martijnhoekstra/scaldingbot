a776fa40852193845f49193928e2ef0235f8e75d
Martijn Hoekstra <martijnhoekstra@gmail.com>
2013-04-20 14:52:37 +0200

    tests for parsing ipv4 addresses

--- main/scala/ipaddress/IPv4Address.scala
+++ main/scala/ipaddress/IPv4Address.scala
@@ -1,5 +1,41 @@
 package ipaddress
+import scala.math
 
-class IPv4Address (ipstring : String) extends IPAddress {
+class IPv4Address(ipstring: String) extends IPAddress {
 
+}
+
+object IPv4Address {
+  def isIpv4Address(address: String) = {
+  }
+
+  def intpow(base: Long, exp: Int) = {
+    (0 until exp).foldLeft(1L)((tot, _) => tot * base)
+  }
+
+  def parseOctet(octet: String): Option[Int] = {
+    try {
+      val asInt = octet.toInt
+      if ((asInt >= 0) && (asInt <= 255)) {
+        Some(asInt)
+      } else {
+        None
+      }
+    } catch {
+      case _: Throwable => None
+    }
+  }
+
+  def toInt(address: String) = {
+    val octets = address.split("""\.""")
+    if (octets.length != 4) None
+    else {
+      val intoctets = (octets map parseOctet).toList.flatten
+      if (intoctets.length != 4) None
+      else {
+        val withOnum = intoctets.reverse.zip(0 until (5))
+        Some(withOnum.map(t => t._1 * intpow(256, t._2)).sum)
+      }
+    }
+  }
 }
